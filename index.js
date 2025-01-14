bb89360a066b78efe430a7056338e7af3097fc84
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2025-01-13 20:43:50 -0400

    resolver functions

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// db
+import db from "./db.js";
+
 // types
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    }
+  }
+};
 
 // server setup
 const server = new ApolloServer({
   typeDefs,
+  resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
