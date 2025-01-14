b89a64df52bf8f8fbc55f06250af3a9d75b0e76d
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2025-01-13 20:56:44 -0400

    Query Variables

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ const resolvers = {
     },
     authors() {
       return db.authors;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id)
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id)
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id)
     }
   }
 };
